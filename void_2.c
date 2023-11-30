07017cb62fd186afb270d312bb881327c1e76ca5
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2023-12-01 00:21:31 +0800

    void_2.c: long or even set of digits up until particular number e.g. all even digits from 1 to 10000000

--- void_2.c
+++ void_2.c
@@ -5,47 +5,72 @@
 const char EVEN[] = "even";
 const char ODD[] = "odd";
 
-void print_odd(long long unsigned int n) {
-  return;
+void print_odd(long long n) {
+  if(n == 0) {
+    printf("\n");
+    return;
+  }
+
+  if(n % 2 == 1) {
+    printf("%lld ", n);
+  } 
+
+  return print_odd(n - 1);
 }
 
-void print_even(long long unsigned int n) {
-  return;
+void print_even(long long n) {
+  if(n == 0) {
+    printf("\n");
+    return;
+  }
+
+  if(n % 2 == 0) {
+    printf("%lld ", n);
+  } 
+
+  return print_even(n - 1);
 }
 
 void usage(char **argv) {
-  printf("\nUsage: odd|even <number>\n", argv[0]);
-  printf("\nExample: even 1000\n\n", argv[0]);
+  printf("\nUsage: %s odd|even <number>\n", argv[0]);
+  printf("\nExample: %s even 1000\n\n", argv[0]);
   return;
 }
 
 void only_odd_or_even_error(char **argv) {
-  printf("Error: only 'odd' or 'even' values are allowed as the first parameter");
+  printf("\nError: only 'odd' or 'even' values are allowed as the first parameter\n\n");
   usage(argv);
   return;
 }
 
+void only_positive_numbers_error() {
+  printf("\nError: number passed is less than zero but only positive numbers allowed\n\n");
+}
+
 void main(int argc, char **argv) {
-  if(argc == 1 || argc >= 3) {
+  if(argc != 3) {
     usage(argv);
     return;
   }
 
   char* _type = argv[1];
 
-  if(_type != EVEN && _type != ODD) {
+  if((strcmp(_type, EVEN) != 0) && (strcmp(_type, ODD) != 0)) {
     only_odd_or_even_error(argv);
     return;
   }
 
-  long long unsigned int n = atoi(argv[2]);
+  long long n = atoi(argv[2]);
 
-  if(_type == EVEN) {
+  if(n <= 0) {
+    only_positive_numbers_error();
+    return;
+  }
+
+  if(strcmp(_type, EVEN) == 0) {
     print_even(n);
-    return;
-  } else if(_type == ODD) {
+  } else {
     print_odd(n);
-    return;
   }
 
   return;
